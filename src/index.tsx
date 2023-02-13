aa6a2180ea528ba5ba7d7c2625a599d426fa68a8
Alejandro Cerezo <alce65@hotmail.es>
2023-02-13 20:26:57 +0100

    🎉 Add base html-css-js

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import app from './app';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <>Loading</>
+  </React.StrictMode>
+);
