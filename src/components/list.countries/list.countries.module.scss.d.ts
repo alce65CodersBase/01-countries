f62ac524dcd7fedb228597991101d123c434d868
Alejandro Cerezo <alce65@hotmail.es>
2023-02-16 10:28:12 +0100

    🏗️ Add conditional rendering when NO countries

--- src/components/list.countries/list.countries.module.scss.d.ts
+++ src/components/list.countries/list.countries.module.scss.d.ts
@@ -1,1 +1,2 @@
 export const countriesList;
+export const noCountries;
--- src/components/list.countries/list.countries.tsx
+++ src/components/list.countries/list.countries.tsx
@@ -1,16 +1,23 @@
 import { BaseCountry } from '../../models/country';
 import { BasicCard } from '../basic.card/basic.card';
-import { countriesList } from './list.countries.module.scss';
+import { countriesList, noCountries } from './list.countries.module.scss';
 
 type ListCountriesProps = { countries: BaseCountry[] };
 export function ListCountries({ countries }: ListCountriesProps) {
   return (
-    <ul className={countriesList}>
-      {countries.map((item) => (
-        <li key={item.id}>
-          <BasicCard country={item}></BasicCard>
-        </li>
-      ))}
-    </ul>
+    <>
+      {' '}
+      {countries.length ? (
+        <ul className={countriesList}>
+          {countries.map((item) => (
+            <li key={item.id}>
+              <BasicCard country={item}></BasicCard>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={noCountries}>No countries: start a search</p>
+      )}
+    </>
   );
 }
