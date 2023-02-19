deaea1ac275a99257dd0a86cf4eec79fce33f5d3
Alejandro Cerezo <alce65@hotmail.es>
2023-02-19 13:49:32 +0100

    ✨ Add component for flag and coat of arms

---
+++ src/components/symbol/symbol.tsx
@@ -0,0 +1,27 @@
+import { Flags } from '../../models/country';
+import { flag, small, middle, large } from './symbol.module.scss';
+
+type SymbolProps = {
+  source: Partial<Flags>;
+  classCSS?: string;
+};
+
+export function Symbol({ source, classCSS = 'small' }: SymbolProps) {
+  if (!source.svg && !source.png) return <></>;
+
+  const cssClasses: { [key: string]: any } = {
+    small,
+    middle,
+    large,
+  };
+
+  return (
+    <figure className={flag}>
+      <img
+        className={cssClasses[classCSS]}
+        src={source.svg ? source.svg : source.png}
+        alt={source.alt || 'Arms'}
+      />
+    </figure>
+  );
+}
