52d07b5a74cbcf9361b6de9b9a72cedb8f1e5538
Alejandro Cerezo <alce65@hotmail.es>
2023-02-25 12:05:43 +0100

    ♻️ Refactor language input as independent component

--- src/components/search.params/search.params.test.tsx
+++ src/components/search.params/search.params.test.tsx
@@ -6,12 +6,14 @@ import { ListCountries } from '../list.countries/list.countries';
 import { searchCountries } from '../../service/repo/countries.api.repo';
 import { useContinentsList } from '../../hooks/use.continents.list';
 import { useRegionLists } from '../../hooks/use.regions.list';
+import { LanguageInput } from '../language.input/language.input';
 
 jest.mock('../../service/repo/countries.api.repo');
 jest.mock('../basic.card/basic.card');
 jest.mock('../list.countries/list.countries');
 jest.mock('../../hooks/use.continents.list');
 jest.mock('../../hooks/use.regions.list');
+jest.mock('../language.input/language.input');
 
 (useContinentsList as jest.Mock).mockReturnValue({
   continents: ['some continent'],
@@ -21,16 +23,20 @@ jest.mock('../../hooks/use.regions.list');
   status: 'loading',
 });
 
+(LanguageInput as jest.Mock).mockImplementation(
+  ({ language, handleLanguage }) => {
+    handleLanguage(language);
+  }
+);
+
 describe('Given SearchParams component rendered', () => {
   let elements: HTMLFormElement[];
-  const mockLanguage = 'some language';
   const mockContinent = 'some continent';
   const mockRegion = 'some region';
 
   beforeEach(async () => {
     await act(async () => render(<SearchParams></SearchParams>));
     elements = [
-      screen.getByRole('textbox'),
       ...screen.getAllByRole<HTMLFormElement>('combobox'),
       screen.getByRole('button'),
     ];
@@ -38,8 +44,9 @@ describe('Given SearchParams component rendered', () => {
 
   describe('When there are still NO data from the user ', () => {
     test('Then form controls should be in the document', () => {
-      expect(elements.length).toBe(4);
+      expect(elements.length).toBe(3);
       elements.forEach((item) => expect(item).toBeInTheDocument());
+      expect(LanguageInput).toHaveBeenCalled();
     });
 
     test('Then ListCountries should be in the document', () => {
@@ -47,54 +54,32 @@ describe('Given SearchParams component rendered', () => {
     });
   });
   describe('When the user use the form', () => {
-    test('A language could be search', async () => {
-      await userEvent.clear(elements[0]);
-      await userEvent.type(elements[0], mockLanguage);
-      expect(elements[0]).toHaveValue(mockLanguage);
+    test('A continent could be search', async () => {
+      await userEvent.selectOptions(elements[0], mockContinent);
+      expect(elements[0]).toHaveValue(mockContinent);
       await act(async () => {
-        fireEvent.click(elements[3]);
-      });
-    });
-
-    test('A language & continent could be search', async () => {
-      await userEvent.clear(elements[0]);
-      await userEvent.type(elements[0], mockLanguage);
-      await userEvent.selectOptions(elements[1], mockContinent);
-      expect(elements[0]).toHaveValue(mockLanguage);
-      expect(elements[1]).toHaveValue(mockContinent);
-      await act(async () => {
-        fireEvent.click(elements[3]);
+        fireEvent.click(elements[2]);
       });
       expect(searchCountries).toHaveBeenCalled();
     });
 
-    test('A language & continent & region could be search', async () => {
-      await userEvent.clear(elements[0]);
-      await userEvent.type(elements[0], mockLanguage);
-      await userEvent.selectOptions(elements[1], mockContinent);
-      await userEvent.selectOptions(elements[2], mockRegion);
-      expect(elements[0]).toHaveValue(mockLanguage);
-      expect(elements[1]).toHaveValue(mockContinent);
-      expect(elements[2]).toHaveValue(mockRegion);
+    test('A continent & region could be search', async () => {
+      await userEvent.selectOptions(elements[0], mockContinent);
+      await userEvent.selectOptions(elements[1], mockRegion);
+      expect(elements[0]).toHaveValue(mockContinent);
+      expect(elements[1]).toHaveValue(mockRegion);
       await act(async () => {
-        fireEvent.click(elements[3]);
+        fireEvent.click(elements[2]);
       });
       expect(searchCountries).toHaveBeenCalled();
     });
 
-    test('A continent could be select', async () => {
-      await userEvent.selectOptions(elements[1], mockContinent);
-      expect(elements[1]).toHaveValue(mockContinent);
-      await act(async () => {
-        fireEvent.click(elements[3]);
-      });
-    });
     test('A region of a continent could be select', async () => {
-      await userEvent.selectOptions(elements[1], mockContinent);
-      await userEvent.selectOptions(elements[2], mockRegion);
-      expect(elements[2]).toHaveValue(mockRegion);
+      await userEvent.selectOptions(elements[0], mockContinent);
+      await userEvent.selectOptions(elements[1], mockRegion);
+      expect(elements[1]).toHaveValue(mockRegion);
       await act(async () => {
-        fireEvent.click(elements[3]);
+        fireEvent.click(elements[2]);
       });
     });
   });
--- src/components/search.params/search.params.tsx
+++ src/components/search.params/search.params.tsx
@@ -1,77 +1,41 @@
-import { SyntheticEvent, useEffect, useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import { searchParams, formGroup, results } from './search.params.module.scss';
-import {
-  getLanguages,
-  searchCountries,
-} from '../../service/repo/countries.api.repo';
+import { searchCountries } from '../../service/repo/countries.api.repo';
 import { BaseCountry } from '../../models/country';
 import { ListCountries } from '../list.countries/list.countries';
 import { useRegionLists } from '../../hooks/use.regions.list';
 import { useContinentsList } from '../../hooks/use.continents.list';
+import { LanguageInput } from '../language.input/language.input';
 
 export const SearchParams = () => {
   // Controlled form for get language, continent & region
   const [language, setLanguage] = useState('');
-
-  const [filteredSuggestions, setFilteredSuggestions] = useState<string[]>([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [allLanguages, setAllLanguages] = useState<string[]>([]);
   const [continent, setContinent] = useState('');
   const [region, setRegion] = useState('');
-
   const { continents } = useContinentsList();
 
   const { regions, status } = useRegionLists(continent);
   const [countries, setCountries] = useState<BaseCountry[]>([]);
+
+  const handleLanguage = (language: string) => {
+    setLanguage(language);
+  };
 
   const handleSubmit = async (ev: SyntheticEvent) => {
     ev.preventDefault();
     setCountries(await searchCountries(language, continent, region));
   };
 
-  const handleChangeLanguage = (ev: SyntheticEvent) => {
-    const { value } = ev.target as HTMLInputElement;
-    setLanguage(value);
-    if (value.length >= 2) {
-      setShowSuggestions(true);
-      const validLanguages = allLanguages.filter((item) =>
-        Boolean(item.toLowerCase().includes(value.toLowerCase()))
-      );
-      setFilteredSuggestions(validLanguages);
-    } else {
-      setShowSuggestions(false);
-    }
-  };
-
-  useEffect(() => {
-    const loadLanguages = async () => {
-      setAllLanguages(await getLanguages());
-    };
-
-    loadLanguages();
-  }, []);
-
   return (
     <section className={searchParams}>
       <header>
         <form onSubmit={handleSubmit}>
           <div className={formGroup}>
-            <label htmlFor="">Language</label>
-            <input
-              placeholder="Type a language in english"
-              name="language"
-              list="languages"
-              value={language}
-              onChange={handleChangeLanguage}
-            />
+            <LanguageInput
+              language={language}
+              handleLanguage={handleLanguage}
+            ></LanguageInput>
           </div>
-          {showSuggestions && (
-            <datalist id="languages">
-              {filteredSuggestions.map((item) => (
-                <option key={item} value={item} />
-              ))}
-            </datalist>
-          )}
           <div className={formGroup}>
             <label htmlFor="continent">Continente</label>
             <select
